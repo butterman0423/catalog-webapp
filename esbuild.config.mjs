730d2d67c5c3d6c4966446eb8b9acc2d18c84700
Nathaniel Escaro <butterman423@gmail.com>
2024-05-21 13:57:04 -0400

    Script rerun support in dev tools

---
+++ esbuild.config.mjs
@@ -0,0 +1,24 @@
+import * as esbuild from 'esbuild';
+import process from 'process'
+
+const opts = {
+    entryPoints: ['./src/app.jsx'],
+    bundle: true,
+    platform: "node",
+    format: "cjs",
+	target: "es2018",
+	logLevel: "info",
+	sourcemap: "inline",
+	treeShaking: true,
+	outfile: "index.js",
+}
+
+
+
+if(process.argv[2] === 'production') {
+	await esbuild.build(opts);
+	process.exit(0);
+}
+
+const ctx = await esbuild.context(opts)
+await ctx.watch();
