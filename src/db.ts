e93f1cfef275f3fb47db23d90fb74fce06df85da
Nathaniel Escaro <butterman423@gmail.com>
2024-05-24 15:17:24 -0400

    Comments and PUT template for / route

--- src/db.ts
+++ src/db.ts
@@ -13,6 +13,8 @@ interface ColumnInfo {
 
 const db = new Database(":memory:");
 
+// --------------------------------------------------------------------------------
+// CONFIGURATION SUBJECT TO CHANGE //
 db.prepare(`CREATE TABLE sample (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     dtype TEXT NOT NULL,
@@ -25,25 +27,42 @@ insert.run("number", "1");
 insert.run("text", "hello");
 insert.run("datetime", "05212024:15:11:00");
 
+// --------------------------------------------------------------------------------
+
 const router = express.Router();
 
 router.use(cors());
 router.use(express.json());
 
-router.get("/", (_req, res) => {
+/** Route / **/
+router.route("/")
+
+// Query data
+.get((_req, res) => {
     const query = db.prepare("SELECT * FROM sample");
     const data = query.all();
 
     res.send(data);
-});
+})
 
-router.post("/", (req, res) => {
+// Inserting new rows
+.post((req, res) => {
+    const stmt = db.prepare(`INSERT INTO sample (dtype, data) VALUES (@dtype, @data)`);
+
     const dat = req.body as {dtype: string, data: string};
-    const ret = insert.run(dat.dtype, dat.data);
+    const ret = stmt.run(dat)
     
     res.send(`${ret.lastInsertRowid}`);
 })
 
+// Updating existing rows
+.put((req, res) => {
+    
+});
+
+/** Route /headers/ **/
+
+// Retrieve valid column names
 router.get("/headers/", (_req, res) => {
     const tinfo = db.pragma('table_info(sample)') as Array<ColumnInfo>;
     const heads = tinfo
