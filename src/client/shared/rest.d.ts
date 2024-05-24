12121f1f9f9b410dac1a05809145493fd24a731d
Nathaniel Escaro <butterman423@gmail.com>
2024-05-24 16:48:23 -0400

    Reset entire frontend

--- src/client/shared/rest.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function get<T,>(url: string): Promise<T>;
-export declare function post<T,>(url: string, dat: T): Promise<PostResponse>;
-export type PostResponse = {
-    row: string,
-    data: any
-}
