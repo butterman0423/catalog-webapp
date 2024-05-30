95403e4e5706a41557a0dbf36f9bf438b38a8acb
Nathaniel Escaro <butterman423@gmail.com>
2024-05-30 14:11:29 -0400

    Local install of Bootstrap v5, DataTables, Jquery

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,18 @@ import root_router from "./routes/root";
 
 const app = express();
 
-app.use( express.static(join(__dirname, "public")) );
-app.use("/home/", root_router);
+app.use( "/", express.static(join(__dirname, "public")) );
+app.use( "/bootstrap/", express.static(join(__dirname, "node_modules/bootstrap/dist")) );
+app.use( "/datatables/", express.static(join(__dirname, "node_modules/datatables.net/js")))
+app.use( "/datatables-bs5/", express.static(join(__dirname, "node_modules/datatables.net-bs5")) );
+app.use( "/jquery/", express.static(join(__dirname, "node_modules/jquery/dist")) );
+app.use( "/home/", root_router );
 
 app.get('/', (_req, res) => {
     res.redirect("/home/");
 })
-const PORT = 3000;
 
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on: http://localhost:${PORT}/`)
 })
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -30,11 +30,9 @@ router.route("/")
         <html>
             <head>
                 <title>Catalog</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" 
-                    crossorigin="anonymous"/>
-                
-                <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap.css"/>
+
+                <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css"/>
+                <link rel="stylesheet" type="text/css" href="/datatables-bs5/css/dataTables.bootstrap5.min.css"/>
                 <link rel="stylesheet" type="text/css" href="/styles/style.css"/>
             </head>
             <body>
@@ -66,12 +64,10 @@ router.route("/")
                     ${ Modal.build({ title: "Add New Row", body: Form.build({ headers: headers }) }) }
                 </div>
                 
-                <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" 
-                    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" 
-                    crossorigin="anonymous"></script>
-                <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
-                <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap.js"></script>
+                <script src="/jquery/jquery.min.js"></script>
+                <script src="/bootstrap/js/bootstrap.min.js"></script>
+                <script src="/datatables/dataTables.min.js"></script>
+                <script src="/datatables-bs5/js/dataTables.bootstrap5.js"></script>
                 <script src="/scripts/home-loader.js"></script>
             </body>
         </html>
