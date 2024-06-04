7592859542881c1a5613899905c4c3863c91e270
Nathaniel Escaro <butterman423@gmail.com>
2024-06-04 17:25:14 -0400

    Editable rows

--- src/client/home/loader.ts
+++ src/client/home/loader.ts
@@ -4,16 +4,36 @@ import 'datatables.net-bs5';
 import './btns';
 import 'datatables.net-select-bs5';
 
+type ModalData = {
+    title: string,
+    url: string,
+    method: string,
+    target?: HTMLElement
+}
 
-function formatModal(ev: JQuery.ClickEvent) {
+function formatModal(dat: ModalData) {
     const modal = $('#db-form-modal');
+    let url = dat.url;
+
+    if(dat.target) {
+        const row = $(dat.target);
+        url += row.data("pk");
+
+        const inputs = $('input.form-input');
+        row.children('td:not(:first-child)')
+        .each((i, el) => {
+            $(inputs.get(i) as HTMLElement)
+            .val($(el).text())
+        });
+    }
     
     modal.find('.modal-title')
-        .text(ev.data.title);
+        .text(dat.title);
     
     modal.find('#db-form-submit')
-        .data('url', ev.data.url)
-        .data('method', ev.data.method);
+        .data('url', url)
+        .data('method', dat.method)
+        .data('editing', dat.target !== undefined);
 }
 
 $(async () => {
@@ -29,7 +49,12 @@ $(async () => {
                 // @ts-expect-error
                 buttons: ['csv', 'excel', 'print']
             }
-        ]
+        ],
+        select: {
+            style: 'single',
+            items: 'row',
+            info: false
+        }
     });
 
     tbl
@@ -40,18 +65,33 @@ $(async () => {
     .appendTo($('.tbl-config'));
 
     // Add/Edit functionality
-    // $('#edit-btn').on('click', {}, formatModal);
+    $('#edit-btn').on('click', {
+        title: "Edit Existing Row",
+        url: "/data/",
+        method: "PUT",
+    }, ({ data }) => {
+        const target = $('#datatable .selected').get(0);
+        if(!target) {
+            alert("Please Select a Row to Edit.")
+        }
+        else {
+            formatModal({target: target, ...data});
+            const modal = Modal.getOrCreateInstance('#db-form-modal');
+            console.log(modal)
+            modal.show();
+        }
+            
+    });
+
     $('#add-btn').on('click', {
-        title: "Add New Row (TEST)",
+        title: "Add New Row",
         url: "/data/",
         method: "POST"
-        // target: number   Row pk to yank data from
-    }, formatModal);
+    }, ({ data }) => formatModal(data));
 
     // Modal form functionality
-    const submitBtn = $('#db-form-submit')
-    submitBtn.on('click', async () => {
-        submitBtn.addClass('disabled');
+    $('#db-form-submit').on('click', async function() {
+        $(this).addClass('disabled');
 
         const fields: {[k: string]: any} = {};
         $('input.form-input').each((_, el) => {
@@ -60,27 +100,40 @@ $(async () => {
         });
 
         try {
-            const pkStr = await $.ajax({
-                url: submitBtn.data('url'),
-                method: submitBtn.data('method'),
+            const res = await $.ajax({
+                url: $(this).data('url'),
+                method: $(this).data('method'),
                 data: JSON.stringify(fields),
                 processData: false,
                 contentType: "application/json",
                 dataType: "text"
             });
 
-            tbl.row
-                .add( [parseInt(pkStr)].concat(Object.values(fields)) )
-                .draw();
+            if($(this).data('editing')) {
+                const vals = Object.values(fields);
+                $('.selected td:not(:first-child)')
+                .each(function(i) {
+                    $(this).text(vals[i]);
+                })
+
+                tbl.row('.selected').deselect();
+            }
+            else {
+                $(  tbl.row
+                    .add( [parseInt(res)].concat(Object.values(fields)) )
+                    .node()
+                ).data('pk', res);
+                tbl.draw();
+            }
             
             //$(...).modal('hide') causes .modal() is not function issue
-            const modal = Modal.getInstance($('#db-form-modal').get(0) as HTMLElement) as Modal;
+            const modal = Modal.getOrCreateInstance('#db-form-modal');
             modal.hide();
         } 
         catch(err) {
             console.log("Request failed", err)
         }
 
-        submitBtn.removeClass('disabled');
+        $(this).removeClass('disabled');
     });
 })
